261802e36eee0e34ef02885ceecd6e25f03c5ec7
John Casey <jdcasey@commonjava.org>
2020-12-23 15:50:53 -0600

    Allow CORS

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const Firestore = require('@google-cloud/firestore');
 const db = new Firestore();
 
 exports.nodelist = async(req, res) => {
+  response.set('Access-Control-Allow-Origin', "*");
+  response.set('Access-Control-Allow-Methods', 'GET');
+
   if (req.method != 'GET') {
     // console.log(`${req.method} requests are not allowed.`);
     return res.status(403).send(`Method ${req.method} is not allowed.`);
