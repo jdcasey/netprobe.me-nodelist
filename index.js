f2854f0788af6e2e1b9b28d38fee1183be862272
John Casey <jdcasey@commonjava.org>
2020-12-23 00:49:53 -0600

    initial implementation to retrieve a list of nodes from the netprobe datasets

---
+++ index.js
@@ -0,0 +1,22 @@
+const Firestore = require('@google-cloud/firestore');
+
+const db = new Firestore();
+
+exports.getNetprobeData = async(req, res) => {
+  if (req.method != 'GET') {
+    // console.log(`${req.method} requests are not allowed.`);
+    return res.status(403).send(`Method ${req.method} is not allowed.`);
+  }
+
+  console.log('Retrieving list of nodes');
+  const snapshot = await db.collection(`netprobe`)
+    .limit(10)
+    .get();
+
+  let data = [];
+  snapshot.forEach(doc => {
+    data.push(doc.data());
+  });
+
+  return res.status(200).send(data);
+};
