940325150c80ce6591c776e9dec76a6882c6dd80
John Casey <jdcasey@commonjava.org>
2020-12-23 01:02:40 -0600

    fix exported function name and add test step to deploy workflow

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Firestore = require('@google-cloud/firestore');
 
 const db = new Firestore();
 
-exports.getNetprobeData = async(req, res) => {
+exports.nodelist = async(req, res) => {
   if (req.method != 'GET') {
     // console.log(`${req.method} requests are not allowed.`);
     return res.status(403).send(`Method ${req.method} is not allowed.`);
